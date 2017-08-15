774631a13f1b7ecf41ed24d563e36d8eca55b6c4
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-15 12:01:01 -0400

    aug15-12:00

--- assets/javascript/giphytrial.js
+++ assets/javascript/giphytrial.js
@@ -1,3 +1,4 @@
+function getStuff() {
 var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
 
     $.ajax({
@@ -6,3 +7,4 @@ var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
     }).done(function(response) {
       console.log(response);
     });
+}
